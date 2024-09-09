1da3a03a16336505091ea7c03c551c352104ab5e
Enni Rajala <enni.rajala@gmail>
2024-09-09 10:56:51 +0300

    save different files removed

--- enni/api.py
+++ enni/api.py
@@ -2,8 +2,6 @@ import requests
 import os
 import pandas as pd
 
-# TODO add country code
-
 def _start_api():
     API_KEY = '7b2fbb569507ae14141d3de0ff76044999ae3859816024ab9d2978c83bc3d804'
 
@@ -29,11 +27,6 @@ def _start_api():
         sensor_data = get_location(sensor, API_KEY, wanted_params)
         save_same_file(sensor_data, wanted_params, save_directory, city[i])
         i = i +1
-
-"""
-        # save data in different csv files grouped by locations
-        save_different_files(sensor_data, wanted_params, save_directory)
-"""
 
 # call api with provided location id
 def get_location(location_id, API_KEY, wanted_params):
@@ -88,40 +81,6 @@ def save_same_file(sensor_data, wanted_params, save_directory, city):
     df = pd.read_csv(file_name)
     df = df.drop_duplicates()
     df.to_csv(file_name, mode='w', index=False)
-"""
-def save_different_files(sensor_data, wanted_params, save_directory):
-    sensor_dict = sensor_data.get('results', [])[0]
-
-    # create row dictionary for storing variables to keep
-    row = {'id': sensor_dict.get('id', ''),
-        'location': sensor_dict.get('name', ''),
-        'lat': sensor_dict.get('coordinates', {}).get('latitude', ''),
-        'lon': sensor_dict.get('coordinates', {}).get('longitude', ''),
-        'day': sensor_dict.get('lastUpdated')[:10],
-        'time': sensor_dict.get('lastUpdated')[11:16],
-        'pm25': '',
-        'pm10': '',
-        'so2': '',
-        'no': '',
-        'o3': ''
-        }
-
-    # update row dict to include available sensors from the ones we're intereted in
-    for param in sensor_dict.get('parameters', []):
-        if param.get("parameterId") in wanted_params:
-                row.update({param['parameter']: param.get('lastValue')})
-
-    # add row to csv file
-    df = pd.DataFrame([row])
-
-    file_name = os.path.join(save_directory, row['location'] + '_sensor_data.csv')
-    df.to_csv(file_name, mode='a', header=not pd.io.common.file_exists(file_name), index=False)
-
-    # remove duplicates
-    df = pd.read_csv(file_name)
-    df = df.drop_duplicates()
-    df.to_csv(file_name, mode='w', index=False)
-"""
 
 #comment this when using airflow
 _start_api()
